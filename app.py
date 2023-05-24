13bfab23f7badeb7bb2cb81298e2961dc504c4b3
alexis perez <alexisperez99@hotmail.fr>
2023-05-24 14:24:44 +0200

    l'api fonctionne

--- app.py
+++ app.py
@@ -1,26 +1,36 @@
 # 1. Library imports
 import uvicorn
 from fastapi import FastAPI
-from model import IrisModel, IrisSpecies
+from model import SentimentModel, ReviewSentiment
 
 # 2. Create app and model objects
-app = FastAPI()
-model = IrisModel()
+app = FastAPI(title= "Sentiment Model API")
+model = SentimentModel()
 
 # 3. Expose the prediction functionality, make a prediction from the passed
 #    JSON data and return the predicted flower species with the confidence
-@app.get('/predict')
+@app.post('/test_predict')
 
-def predict_species(iris: IrisSpecies):
-    data = iris.dict()
-    prediction, probability = model.predict_species(
-        data['sepal_length'], data['sepal_width'], data['petal_length'], data['petal_width']
+def predict_sentiment(review: ReviewSentiment):
+    data = review.dict()
+    prediction, probability = model.predict_sentiment(
+        data['review']
     )
     return {
         'prediction': prediction,
         'probability': probability
     }
+@app.get('/predict')
 
+def predict_sentiment(review: ReviewSentiment):
+    data = review.dict()
+    prediction, probability = model.predict_sentiment(
+        data['review']
+    )
+    return {
+        'prediction': prediction,
+        'probability': probability
+    }
 # # 4. Run the API with uvicorn
 # #    Will run on http://127.0.0.1:8000
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -3,29 +3,26 @@ import joblib
 
 
 # 2. Class which describes a single flower measurements
-class IrisSpecies(BaseModel):
-    sepal_length: float
-    sepal_width: float
-    petal_length: float
-    petal_width: float
+class ReviewSentiment(BaseModel):
+    review : str
 
 
 # 3. Class for training the model and making predictions
-class IrisModel:
+class SentimentModel:
     # 6. Class constructor, loads the dataset and loads the model
     #    if exists. If not, calls the _train_model method and
     #    saves the model
     def __init__(self):
-        self.model_fname_ = 'iris_model.pkl'
+        self.model_fname_ = 'baseline.pkl'
         self.model = joblib.load(self.model_fname_)
 
     # 5. Make a prediction based on the user-entered data
     #    Returns the predicted species with its respective probability
-    def predict_species(self, sepal_length, sepal_width, petal_length, petal_width):
-        data_in = [[sepal_length, sepal_width, petal_length, petal_length]]
-        prediction = self.model.predict(data_in)
-        probability = self.model.predict_proba(data_in).max()
-        return prediction[0], probability
+    def predict_sentiment(self, review):
+        
+        prediction = self.model.predict([review])
+        probability = self.model.predict_proba([review]).max()
+        return  prediction[0].item(), probability.item()
 
 
 
