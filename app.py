1919ade774bc7c30f1c4f2ca32b8cc21ce651c1b
maxime lu <merdeth69@gmail.com>
2023-05-24 10:46:58 +0200

    test

--- app.py
+++ app.py
@@ -1,16 +1,67 @@
 # 1. Library imports
 import uvicorn
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import pymysql
+from TableStruct import bddinputs,  bddtest
+from typing import List
+from urllib.parse import urlparse
+from dotenv import load_dotenv
+import os
 from model import IrisModel, IrisSpecies
 
 # 2. Create app and model objects
 app = FastAPI()
 model = IrisModel()
 
+# Charger les variables d'environnement à partir du fichier .env
+load_dotenv()
+
+# Récupérer l'URL de la base de données à partir des variables d'environnement
+database_url = os.getenv("DATABASE_URL")
+
+# Extraire les composants de l'URL de la base de données
+url_components = urlparse(database_url)
+db_host = url_components.hostname
+db_user = url_components.username
+db_password = url_components.password
+db_name = url_components.path.strip("/")
+
+# Configurer la connexion à la base de données MySQL
+conn = pymysql.connect(
+    host=db_host,
+    user=db_user,
+    password=db_password,
+    database=db_name
+)
+
+#affichage pr test
+@app.get("/")
+async def get_items() -> List[bddtest]:
+    # Effectuer des opérations sur la base de données
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT * FROM test")
+        results = cursor.fetchall()
+
+    # Convertir les résultats en une liste d'objets Test a refacto par la suite
+    items = []
+    for row in results:
+        data_dict = {
+            "id": row[0],
+            "review" : row[1],
+            "rating" : row[2],
+        }
+
+        data_user = bddtest(**data_dict)
+        items.append(data_user)
+
+    # Retourner les résultats de l'API
+    return items
+
+
 # 3. Expose the prediction functionality, make a prediction from the passed
 #    JSON data and return the predicted flower species with the confidence
 @app.get('/predict')
-
 def predict_species(iris: IrisSpecies):
     data = iris.dict()
     prediction, probability = model.predict_species(
@@ -20,6 +71,19 @@ def predict_species(iris: IrisSpecies):
         'prediction': prediction,
         'probability': probability
     }
+
+ #a changer
+@app.post("/add")
+async def create_item(item: bddinputs):
+    # Effectuer des opérations sur la base de données
+    with conn.cursor() as cursor:
+        query = "INSERT INTO inputs (QUARTER, MONTH, DAY_OF_MONTH) " \
+                 "VALUES (%s, %s, %s)"
+        values = (item.QUARTER,item.MONTH, item.DAY_OF_MONTH)
+        cursor.execute(query, values)
+        conn.commit()
+
+    return {"message": "Item created successfully"}
 
 # # 4. Run the API with uvicorn
 # #    Will run on http://127.0.0.1:8000
